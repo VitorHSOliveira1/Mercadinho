77af1918457069ef385fb5abff5dd3051620579f
Vitor Hugo <vitorhsoliveira13@gmail.com>
2024-10-05 21:19:29 -0300

    adhuwaudhaoufhaw

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-const botaoCompraAnaBrava = document.getElementById('comprarAnaBrava');
-const botaoComprarAnaFeliz = document.getElementById('comprarAnaFeliz');
-const botaoComprarAnaIndiferente = document.getElementById('comprarAnaIndiferente');
-const botaoComprarRiosBravo = document.getElementById('comprarRiosBravo');
-const botaoComprarRiosFeliz = document.getElementById('comprarRiosFeliz');
+const botaoCompraAnaBrava = document.querySelector('#comprarAnaBrava');
+const botaoComprarAnaFeliz = document.querySelector('#comprarAnaFeliz');
+const botaoComprarAnaIndiferente = document.querySelector('#comprarAnaIndiferente');
+const botaoComprarRiosBravo = document.querySelector('#comprarRiosBravo');
+const botaoComprarRiosFeliz = document.querySelector('#comprarRiosFeliz');
+const precoContainer = document.querySelector('#preco-carrinho')
+const nomeContainer = document.querySelector('#nome-carrinho')
+const imgContainer = document.querySelector('#img-carrinho')
+
+const imgAnaBrava = document.createElement("img")
+const imgAnaFeliz = document.createElement("img")
+const imgAnaIndiferente = document.createElement("img")
+const imgRiosBravo = document.createElement("img")
+const imgRiosFeliz = document.createElement("img")
+imgAnaBrava.src = "images/Ana-Bravinha.jpg"
+imgAnaFeliz.src = "imgaes/Ana-Felizinha.jpg"
+imgAnaIndiferente.src = "images/Ana-mehzinha.jpg"
+imgRiosBravo.src = "images/Rios-bravinho.jpg"
+imgRiosFeliz.src = "images/Rios-felizinho.jpg"
+
 
 const carrinho = [];
 
-function addToCarrinho(nome, preco) {
+function addToCarrinho(nome, preco, img) {
     const produto = {
         nome: nome,
-        preco: preco
+        preco: preco,
+        imagem: img
     };
     carrinho.push(produto); // Adiciona o objeto ao array
     console.log(carrinho); // Mostra o carrinho no console (para fins de teste)
@@ -21,13 +37,7 @@ function displayCartItems() {
     cartContainer.innerHTML = ''; // clear the container
 
     carrinho.forEach((item) => {
-        const cartItemHTML = `
-            <div>
-                <h2>${item.nome}</h2>
-                <p>Preço: ${item.preco} R$</p>
-                
-            </div>
-        `;
+        const cartItemHTML =  
         cartContainer.innerHTML += cartItemHTML;
     });
 }
@@ -35,7 +45,7 @@ function displayCartItems() {
 // Associar os botões com a função de adicionar ao carrinho
 // Associar os botões com a função de adicionar ao carrinho
 botaoCompraAnaBrava.addEventListener("click", () => {
-    addToCarrinho('Ana Brava', 120.99);
+    addToCarrinho('Ana Brava', 120.99, );
     displayCartItems(); // call displayCartItems after adding to cart
 });
 botaoComprarAnaFeliz.addEventListener("click", () => {
@@ -54,3 +64,4 @@ botaoComprarRiosFeliz.addEventListener("click", () => {
     addToCarrinho('Rios Feliz', 125.30);
     displayCartItems();
 });
+BV
